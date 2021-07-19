129e51659076db69afb9194b6ef12706b4982dd7
Felipe Quezada <fquezada@live.cl>
2021-07-19 16:31:04 -0400

    Delete test_exiftool_2.py

--- test_exiftool_2.py
+++
@@ -1,8 +0,0 @@
-from exiftool_helper import ExifTool
-
-with ExifTool() as e:
-    get_tags = e.get_metadata('esteganografia_tarea.jpg')
-    print(get_tags)
-    metadata = e.set_metadata('-Comment=prueba','esteganografia_tarea.jpg')
-    get_tags = e.get_metadata('esteganografia_tarea.jpg')
-    print(get_tags)
